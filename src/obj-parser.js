f781f8794f1c4d72f874e7f30082698b24054c9e
carsonritchey <ritcheycarson@gmail.com>
2025-02-11 21:34:16 -0600

    obj-parser start

---
+++ src/obj-parser.js
@@ -0,0 +1,97 @@
+async function getFileText(path) {
+    if(!path.endsWith('.obj')) console.log('obj-parser: warning: path \'' + path + '\' is not an .obj file!');
+
+    try {
+        const response = await fetch(path);
+        return response.text();
+    } catch(e) {
+        console.log('obj-parser: failed to read file \'' + path + '\'. (does it exist?)');
+    }
+}
+
+async function getObjData(path) {
+    const text  = await getFileText(path);
+    const lines = text.split('\n');
+
+    let name = "";
+
+    // 0th element initialized since .obj indexes starting at 1
+    const vertices = [0, 0, 0];
+    const texcoords = [0, 0];
+    const normals = [0, 0, 0];
+
+    const vertexIndexes = [];
+    const normalsIndexed = [];
+    
+    for(let lineIndex = 0; lineIndex < lines.length; lineIndex++) {
+        const line = lines[lineIndex];
+        if(line[0] == '#' || !line) continue;
+        const [command, ...args] = line.split(/\s+/);
+        
+        if(command == 'v') {
+            if(args.length != 3) {
+                console.log('obj-parser: line', lineIndex + 1, 'with unexpected number of verticies. skipping...');
+                continue;
+            }
+            vertices.push(parseFloat(args[0]));
+            vertices.push(parseFloat(args[1]));
+            vertices.push(parseFloat(args[2]));
+        }
+        else if(command == 'vn') {
+            if(args.length != 3) {
+                console.log('obj-parser: line', lineIndex + 1, 'with unexpected number of normals. skipping...');
+                continue;
+            }
+            normals.push(parseFloat(args[0]));
+            normals.push(parseFloat(args[1]));
+            normals.push(parseFloat(args[2]));
+        }
+        else if(command == 'vt') {
+            if(args.length != 2) {
+                console.log('obj-parser: line', lineIndex + 1, 'with unexpected number of texcoords. skipping...');
+                continue;
+            }
+            texcoords.push(parseFloat(args[0]));
+            texcoords.push(parseFloat(args[1]));
+        }
+        else if(command == 'f') { // v/t/n
+            if(args.length == 3) { // pushes 3 new vertices
+                for(const arg of args) {
+                    const indices = arg.split('/').map(n => parseInt(n));
+                    vertexIndexes.push(indices[0]); // only taking vertex index for now
+                }
+            } else if(args.length == 4) { // pushes 6 new vertices
+                const quadIndices = [];
+                const textIndices = [];
+                const normIndices = [];
+                for(const arg of args) {
+                    const indices = arg.split('/').map(n => parseInt(n));
+                    quadIndices.push(indices[0]);
+                    textIndices.push(indices[1]);
+                    normIndices.push(indices[2]);
+                }
+                vertexIndexes.push(quadIndices[0], quadIndices[1], quadIndices[2]);
+                vertexIndexes.push(quadIndices[0], quadIndices[2], quadIndices[3]);
+
+                console.log(normIndices);
+                // every normal of each vertex on a face
+                for(const index of normIndices) {
+                    console.log(normals[index * 3], normals[index * 3 + 1], normals[index * 3 + 2]);
+                }
+            } else {
+                console.log('obj-parser.js: line', lineIndex + 1, 'with unexpected number of face indices (not a tri or quad). skipping...');
+                continue;
+            }
+        }
+        else if(command == 'o') {
+            name = args.join(" ");
+        }
+        else {
+            console.log('unhandled command \'' + command + '\' with args:', args);
+        }
+    }
+
+    for(let i = 0; i < vertexIndexes.length; i++) normalsIndexed.push(1, 1, 1);
+
+    return { name, vertices, normals, texcoords, vertexIndexes, normalsIndexed };
+}
