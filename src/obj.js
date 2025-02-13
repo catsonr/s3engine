81cb94ea5ac31ce0b8d75b9b39c3028a301f3741
carsonritchey <ritcheycarson@gmail.com>
2025-02-13 15:15:08 -0600

    obj start

---
+++ src/obj.js
@@ -0,0 +1,30 @@
+class Obj {
+    static objs = new Map();
+
+    constructor(pos = [0, 0, 0], scale = [1, 1, 1]) {
+        this.pos = vec3.fromValues(...pos);
+        this.scale = vec3.fromValues(...scale);
+
+        this.matrix = mat4.create();
+        mat4.identity(this.matrix);
+        mat4.translate(this.matrix, this.matrix, this.pos);
+        mat4.scale(this.matrix, this.matrix, this.scale);
+
+        this.data = null;
+    }
+
+    static async loadObj(path) {
+        if (this.objs.has(path)) {
+            return this.objs.get(path);
+        }
+
+        const obj = await getObjData(path);
+        this.objs.set(path, obj);
+
+        return this.objs.get(path);
+    }
+
+    setObjData(path) {
+        this.data = Obj.objs.get(path);
+    }
+}
